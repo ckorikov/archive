dcef8fc052c5b0f330d73801002895f70410a7c8
Constantine Korikov <constantine.korikov+github@gmail.com>
2021-11-20 23:39:08 +0800

    feat(generator): increase environment variables

--- generator/environment.py
+++ generator/environment.py
@@ -1,8 +1,13 @@
+import os
 import sys
 
 
-def get_work_dir():
-    return sys.argv[1] if len(sys.argv) > 1 else '.'
+def get_output_dir():
+    return os.path.abspath(sys.argv[1]) if len(sys.argv) > 1 else os.getcwd()
+
+
+def get_root_dir():
+    return os.path.abspath(os.path.dirname(__file__))
 
 
 def get_template(path, filename='index.template.html'):
